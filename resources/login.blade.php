9baf2736f6f25446f235d5d413aa5b5091767c7a
Jokerchara <taegds@gmail.com>
2023-03-02 16:26:46 +0700

    push

---
+++ resources/login.blade.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Cuba admin is super flexible, powerful, clean &amp; modern responsive bootstrap 5 admin template with unlimited possibilities.">
+    <meta name="keywords" content="admin template, Cuba admin template, dashboard template, flat admin template, responsive admin template, web app">
+    <meta name="author" content="pixelstrap">
+    <link rel="icon" href="../assets/images/favicon.png" type="image/x-icon">
+    <link rel="shortcut icon" href="../assets/images/favicon.png" type="image/x-icon">
+    <title>Cuba - Premium Admin Template</title>
+    <!-- Google font-->
+    <link href="https://fonts.googleapis.com/css?family=Rubik:400,400i,500,500i,700,700i&amp;display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i,900&amp;display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="../assets/css/font-awesome.css">
+    <!-- ico-font-->
+    <link rel="stylesheet" type="text/css" href="../assets/css/vendors/icofont.css">
+    <!-- Themify icon-->
+    <link rel="stylesheet" type="text/css" href="../assets/css/vendors/themify.css">
+    <!-- Flag icon-->
+    <link rel="stylesheet" type="text/css" href="../assets/css/vendors/flag-icon.css">
+    <!-- Feather icon-->
+    <link rel="stylesheet" type="text/css" href="../assets/css/vendors/feather-icon.css">
+    <!-- Plugins css start-->
+    <!-- Plugins css Ends-->
+    <!-- Bootstrap css-->
+    <link rel="stylesheet" type="text/css" href="../assets/css/vendors/bootstrap.css">
+    <!-- App css-->
+    <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+    <link id="color" rel="stylesheet" href="../assets/css/color-1.css" media="screen">
+    <!-- Responsive css-->
+    <link rel="stylesheet" type="text/css" href="../assets/css/responsive.css">
+  </head>
+  <body>
+    <!-- login page start-->
+    <div class="bg-image" 
+        style="background-image: url('../assets/images/login/sign_in.png'); 
+        background-size: cover; 
+        height: 100vh;
+        background-position: center center; 
+        background-repeat: no-repeat;">
+      <div class="container-fluid p-0">
+            <div class="login-card">
+              <div>
+                <div>
+                  <img src="../assets/images/logo/login.png" alt="loginpage" width="507.22" 
+                  height="91.52">
+                  </div>
+
+                    <div class="login-main mt-4"> 
+                  <form class="theme-form">
+                    <h4 class="login-page">Sign in</h4>
+                    <div class="input-group mb-3 mt-4">
+                      <span class="input-group-text" id="basic-addon1">@</span>
+                      <input type="email" class="form-control" required="" placeholder="Test@gmail.com"/>
+                    </div>
+                    
+                    <!-- <div class="form-group">
+                      <div class="form-input position-relative">
+                        <span class="input-group-text" id="basic-addon1">@</span>
+                        <input class="form-control" type="password" name="login[password]" required="" placeholder="***">
+                        <div class="show-hide"><span class="show">                        </span></div>
+                      </div>
+                    </div> -->
+                    
+                    <div class="input-group form-input position-relative mb-3 mt-4">
+                      <span class="input-group-text" id="basic-addon1">@</span>
+                      <input class="form-control" type="password" name="login[password]" required="" placeholder="***"/>
+                      <!-- <div class="show-hide"><span class="show"></span></div> -->
+                    </div>
+
+                      <div class="text-end mt-4">
+                        <button class="btn btn-primary btn-block login-page-btn w-100" type="submit">Sign in</button>
+                      </div>
+              </div>
+            
+            </div>
+
+          </div>
+          <div style="margin-top: -80px;">
+            <p class="text-center login-page-footer" >©2022 DCC by SKIN Indonesia</p>
+          </div>
+</div>
+      <!-- latest jquery-->
+      <script src="../assets/js/jquery-3.5.1.min.js"></script>
+      <!-- Bootstrap js-->
+      <script src="../assets/js/bootstrap/bootstrap.bundle.min.js"></script>
+      <!-- feather icon js-->
+      <script src="../assets/js/icons/feather-icon/feather.min.js"></script>
+      <script src="../assets/js/icons/feather-icon/feather-icon.js"></script>
+      <!-- scrollbar js-->
+      <!-- Sidebar jquery-->
+      <script src="../assets/js/config.js"></script>
+      <!-- Plugins JS start-->
+      <!-- Plugins JS Ends-->
+      <!-- Theme js-->
+      <script src="../assets/js/script.js"></script>
+      <!-- login js-->
+      <!-- Plugin used-->
+    </div>
+  </body>
+</html>
